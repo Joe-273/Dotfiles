af69e01f221bf7963cf0dd1784ed3f9ac99bccf4
Qihao.PC <QihaoWei.273@gmail.com>
2024-11-18 17:17:51 +0800

    Update

--- MacConf/mac_setup.sh
+++ MacConf/mac_setup.sh
@@ -2,7 +2,11 @@ echo ">>>>>> Mac Setup START >>>>>>"
 
 export MAC_CONF_DIR="$(pwd)"
 
-"$MAC_CONF_DIR/bin/update_bomebrew.sh"
+for script in "$MAC_CONF_DIR/bin/"*.sh; do
+    chmod +x "$script"
+done
+
+"$MAC_CONF_DIR/bin/update_homebrew.sh"
 "$MAC_CONF_DIR/bin/update_ohmyzsh.sh"
 "$MAC_CONF_DIR/bin/install_apps.sh"
 "$MAC_CONF_DIR/bin/set_symlink.sh"
