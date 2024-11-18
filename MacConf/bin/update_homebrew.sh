c6d40ab8736f9f3f24d768c5ca9b52130fa1a798
Qihao.PC <QihaoWei.273@gmail.com>
2024-11-18 12:12:50 +0800

    Update MacConf/bin/** & Add zshrc file

--- MacConf/bin/update_homebrew.sh
+++ MacConf/bin/update_homebrew.sh
@@ -1,20 +1,18 @@
-#!/bin/zsh
-
 if command -v brew >/dev/null 2>&1; then
-    echo "Homebrew is already installed"
-    echo "Updating..."
+    echo ">>> Homebrew is already installed"
+    echo ">>> Updating..."
     brew update && brew upgrade
-    echo "Homebrew has been updated!"
+    echo ">>> Homebrew has been updated!"
 else
-    echo "Homebrew is not installed"
-    echo "Installing now..."
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-    echo "Homebrew installation completed!"
+    echo ">>> Homebrew is not installed"
+    echo ">>> Installing now..."
+    /bin/zsh -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    echo ">>> Homebrew installation completed!"
 fi
 
 if command -v brew >/dev/null 2>&1; then
-    echo "Homebrew is ready to use."
+    echo ">>> Homebrew is ready to use."
 else
-    echo "Homebrew setup failed. Please check manually."
+    echo "!!! Homebrew setup failed. Please check manually."
     exit 1
 fi
--- MacConf/bin/update_ohmyzsh.sh
+++ MacConf/bin/update_ohmyzsh.sh
@@ -1,20 +1,29 @@
-#!/bin/bash
-
 OH_MY_ZSH_DIR="$HOME/.oh-my-zsh"
 
 if [ -d "$OH_MY_ZSH_DIR" ]; then
-    echo "Oh My Zsh is already installed"
-    echo "Updating..."
+    echo ">>> Oh My Zsh is already installed"
+    echo ">>> Updating..."
     ZSH="$OH_MY_ZSH_DIR" sh "$OH_MY_ZSH_DIR/tools/upgrade.sh"
 else
-    echo "Oh My Zsh is not installed"
-    echo "Installing now..."
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    echo ">>> Oh My Zsh is not installed"
+    echo ">>> Installing now..."
+    /bin/zsh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi
 
 if [ -d "$OH_MY_ZSH_DIR" ]; then
-    echo "Oh My Zsh installation or update completed successfully!"
+    echo ">>> Oh My Zsh installation or update completed successfully!"
 else
-    echo "Oh My Zsh installation or update failed."
+    echo "!!! Oh My Zsh installation or update failed."
+    exit 1
+fi
+
+# Clone oh-my-zsh plugins to local
+if [ -d "$OH_MY_ZSH_DIR" ]; then
+    echo ">>> Oh My Zsh installation or update completed successfully!"
+    echo ">>> Installing plugins..."
+    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+else
+    echo "!!! Oh My Zsh installation or update failed."
     exit 1
 fi
