563b55e8c4476d2970cd28abc48257a89b2b98f0
Qihao.PC <QihaoWei.273@gmail.com>
2024-11-18 13:22:59 +0800

    Update bin/update_ohmyzsh.sh

--- MacConf/bin/update_ohmyzsh.sh
+++ MacConf/bin/update_ohmyzsh.sh
@@ -23,6 +23,7 @@ if [ -d "$OH_MY_ZSH_DIR" ]; then
     echo ">>> Installing plugins..."
     git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
     git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+    echo ">>> Plugins download completed!"
 else
     echo "!!! Oh My Zsh installation or update failed."
     exit 1
